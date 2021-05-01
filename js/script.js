fa9c9641e82e14060b912738bcc2709805dbd2ef
lukoard <81273639+lukoard@users.noreply.github.com>
2021-05-01 18:40:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -90,3 +90,49 @@ $(document).on('click', '#closesession', function() {
     $('#closesession').hide();
     $('#welcome').hide();
 });
+function grabPoke() {
+  let query = $("#text").val().toLowerCase(); 
+  fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+    .then(response => {
+      if(response.ok) {
+        
+          return response.json()
+      } else {
+        
+        alert('¡Por favor verifica que el nombre del pokemon o el número nacional sean correctos!.')
+      }
+  })
+    .then((out) => {
+     console.log(out)
+    
+  let number = out.id;  
+  let name = out.species.name.toUpperCase();
+  let pic = out.sprites.front_default;
+  let type = out.types.map((type) => type.type.name).join(', ').toUpperCase();
+  let abilities = out.abilities.map((ability) => ability.ability.name).join(', ').toUpperCase();
+  let hp = out.stats[5].base_stat;
+  let attack = out.stats[4].base_stat;
+  let defense = out.stats[3].base_stat;
+  let speed = out.stats[0].base_stat; 
+    document.getElementById("number").innerHTML = "# " + number;
+    document.getElementById("poke_pic").src = pic;
+    document.getElementById("name").innerHTML = name;
+    document.getElementById("type").innerHTML = "Tipo: " + type;
+    document.getElementById("abilities").innerHTML = "Habilidades: " + abilities;
+    document.getElementById("hp").innerHTML = "Salud base: " + hp;
+    document.getElementById("attack").innerHTML = "Ataque: " + attack;
+    document.getElementById("defense").innerHTML = "Defensa: " + defense;
+    document.getElementById("speed").innerHTML = "Velocidad: " + speed;
+  })   
+  }
+  $("#text").keyup(function(event) {
+    if (event.keyCode === 13) {
+        $("#submit").click();
+    }
+});
+      document.getElementById('submit').addEventListener("click", function()
+  {
+      
+      grabPoke();
+    })
+}	
